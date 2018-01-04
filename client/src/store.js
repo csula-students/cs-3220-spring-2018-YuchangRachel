b1f9f17bd0a5162cdcfd13abafbe4d07b6f691f0
Eric Liao <rcliao01@gmail.com>
2018-01-04 09:08:45 -0800

    chore: update docs to use jsdoc pattern

--- client/src/store.js
+++ client/src/store.js
@@ -1,35 +1,59 @@
 // Store is heavily inspired by Redux from React pattern to handle state management
 export default class Store {
+    /**
+     * Create a new store with specific reducer and optionally initial state
+     * @constructor
+     * @param {function} reducer - the reducer to mutate state
+     * @param {initialState} object - the initial state
+     */
 	constructor (reducer, initialState = {}) {
 		this.reducer = reducer;
 		// listeners are internal state to keep track of which reducers to call in this
 		// order
 		this.listeners = [];
-		// initial state
+		// initial internal state
 		this.__state = initialState;
 	}
 
-	// READ only state
+	/**
+	 * Overwrites getter for `state` variable to be READ-ONLY state through
+	 * deepCopy method
+	 */
 	get state () {
 		return deepCopy(this.__state);
 	}
 
-	// only way to change state is by dispatching actions and go through reducer
+	/**
+	 * `dispatch` controls state changes. One should use only dispatch to show
+	 * intent to change state and go through reducer to change so
+	 * @param {object} action - action should contain both `type` and `payload`
+	 */
 	dispatch (action) {
 		this.__state = this.reducer(this.state, action);
-		this.listeners.forEach(l => l(this.state));
+		this.listeners.forEach(l => l(this.state, action));
 	}
 
-	// Observable pattern to listen for changes
+	/**
+	 * subscribe allows consumer to listen for state changes
+	 * @param {function} listener - function contains state and action as its arguments
+	 */
 	subscribe (listener) {
 		this.listeners.push(listener);
 	}
+
+	/**
+	 * unsubscribe allows consumer to stop listening to state changes
+	 * @param {function} listener - the function consumer subscribed earlier
+	 */
 	unsubscribe (listener) {
 		this.listeners = this.listeners.filter(l => l != listener);
 	}
 }
 
-// deepCopy function is to create a immutability of certain object
+/**
+ * deepCopy is helper method to create deep copy of certain object
+ * @param {object} obj - the object to copy from
+ */
 function deepCopy(obj) {
 	return JSON.parse(JSON.stringify(obj));
 }
