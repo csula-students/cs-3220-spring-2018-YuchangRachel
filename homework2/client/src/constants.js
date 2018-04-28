a210e045164a7453c6d20cad7062cf6bb332659a
Yuchang He <phoebee08@gmail.com>
2018-04-27 21:40:10 -0700

    remove directory

--- homework2/client/src/constants.js
+++
@@ -1,9 +0,0 @@
-export default {
-	growthRatio: 0.05,
-	actions: {
-		BUY_GENERATOR: 'BUY_GENERATOR',
-		EXAMPLE: 'EXAMPLE_MUTATION',
-		INCREMENT: 'INCREMENT',
-		CHECK_STORY: 'CHECK_STORY'
-	}
-};
--- homework2/src/main/java/hello/SimpleMath.java
+++
@@ -1,7 +0,0 @@
-package hello;
-
-public class SimpleMath {
-	public static int add (int a, int b) {
-		return a + b;
-	}
-}
